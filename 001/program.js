702c7e70ad5c7fbe30abfb60a871ca7e6dd8590f
Musale Martin <martinmshale@gmail.com>
2017-05-31 10:35:33 +0300

    Challenge eight down

---
+++ 001/program.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const bl = require("bl");
+url = process.argv[2]
+
+http.get(url, function(response) {
+  response.pipe(bl(function(err, data) {
+    if (err) {
+      return console.error(err);
+    } else {
+      data = data.toString()
+      console.log(data.length);
+      console.log(data);
+    }
+  }))
+}).on("error", console.error);

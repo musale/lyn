e531dc78f69254ba9969386f310acae7b722e263
musale <martinmshale@gmail.com>
2017-06-11 22:31:42 +0300

    Implement readable stream.
    Read the alphabet and when I reach Z, I push null to the stream to signify end of reading in data.

--- streams/readableStream.js
+++ streams/readableStream.js
@@ -1,2 +1,12 @@
 const { Readable } = require('stream');
-const inStream = new Readable({});
+const inStream = new Readable({
+    read(size) {
+        this.push(String.fromCharCode(this.currentCharCode++));
+        if (this.currentCharCode > 90) {
+            this.push(null);
+        }
+    }
+});
+
+inStream.currentCharCode = 65;
+inStream.pipe(process.stdout);
